c10101386a0dec5c0e7888bee3fafa78d1a51f0f
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2017-02-08 10:57:56 +0100

    replace npm script with proper globally installable CLI tool

---
+++ bin/ensuite-present.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+'use strict'
+
+const path = require('path')
+const http = require('http')
+const nodeStatic = require('node-static')
+
+const PORT = 4242
+
+process.title = path.basename(__filename, '.js')
+
+const fileServer = new nodeStatic.Server()
+
+http
+  .createServer((request, response) => {
+    request
+      .addListener('end', () => fileServer.serve(request, response))
+      .resume()
+  })
+  .listen(PORT)
+
+console.log(`ensuite-present available on http://localhost:${PORT}`)
